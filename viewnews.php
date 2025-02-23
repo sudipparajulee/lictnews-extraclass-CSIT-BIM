91cde50c49dce0686db15b4f95419335812f3221
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-02-23 12:41:33 +0545

    viewnews

---
+++ viewnews.php
@@ -0,0 +1,26 @@
+<?php include 'header.php'; ?>
+<div class="px-20">
+    <div class="grid grid-cols-4 gap-4">
+        <div class="col-span-3 p-2">
+            <img src="uploads/1.jpg" alt="" class="w-full h-96 object-cover">
+            <h1 class="font-bold text-3xl">News Title</h1>
+            <p class="text-sm text-red-500">2021-09-09</p>
+            <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div>
+            <h1 class="font-bold text-2xl pt-2">Related News</h1>
+            <hr class="border-2 border-blue-400">
+            <div class="grid gap-4 mt-4">
+                <a href="" class="p-2 shadow-sm rounded-lg bg-gray-100 flex gap-4 hover:shadow-md">
+                    <img src="uploads/1.jpg" alt="" class="w-20 h-20 object-cover">
+                    <h1 class="font-bold text-lg">News Title</h1>
+                </a>
+                <a href="" class="p-2 shadow-sm rounded-lg bg-gray-100 flex gap-4 hover:shadow-md">
+                    <img src="uploads/1.jpg" alt="" class="w-20 h-20 object-cover">
+                    <h1 class="font-bold text-lg">News Title</h1>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include 'footer.php'; ?>
