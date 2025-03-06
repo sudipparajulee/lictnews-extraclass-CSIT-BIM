089c4667f062bb5620184cbf0deba06a91f228e4
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-03-06 12:38:41 +0545

    upasets

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -2,13 +2,23 @@
 $qry = "SELECT count(id) as total_notices FROM notices";
 $qrycat = "SELECT count(id) as total_categories FROM categories";
 $qrynews = "SELECT count(id) as total_news FROM news";
+$qrypie = "SELECT count(news.id) as total_news, categories.name FROM news JOIN categories ON news.category_id = categories.id GROUP BY categories.name";
 include 'dbconnection.php';
 $resultnotice = mysqli_query($conn, $qry);
 $resultcat = mysqli_query($conn, $qrycat);
 $resultnews = mysqli_query($conn, $qrynews);
+$resultpie = mysqli_query($conn, $qrypie);
+
 $rownotice = mysqli_fetch_assoc($resultnotice);
 $rowcat = mysqli_fetch_assoc($resultcat);
 $rownews = mysqli_fetch_assoc($resultnews);
+$piecat = [];
+$piedata = [];
+while($rowpie = mysqli_fetch_assoc($resultpie))
+{
+    $piecat[] = $rowpie['name'];
+    $piedata[] = $rowpie['total_news'];
+}
 ?>
             <h1 class="font-bold text-3xl">Dashboard</h1>
             <hr class="h-1 bg-red-600">
@@ -25,5 +35,25 @@ $rownews = mysqli_fetch_assoc($resultnews);
                     <h1 class="font-bold text-xl">Total Notices</h1>
                     <p class="text-4xl font-bold text-right"><?php echo $rownotice['total_notices'] ?></p>
                 </div>
+                <div>
+                    <canvas id="myChart"></canvas>
+                </div>
             </div>
+            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels: <?php echo json_encode($piecat) ?>,
+      datasets: [{
+        label: 'No of News',
+        data: <?php echo json_encode($piedata) ?>,
+        borderWidth: 1
+      }]
+    }
+  });
+</script>
 <?php include 'footer.php'; ?>
